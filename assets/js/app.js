b6a0bdeeff6d501d137dc2ddfa986b006bdc3af6
H0RNETT <antoncd1231405042@gmail.com>
2022-03-14 11:21:40 +0300

    123

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,24 @@
-$(function() {
 
-    $(window).on("scroll", function(){
+// Событие скролла //
 
+    window.addEventListener("scroll", function(){
+        let scrollPoss = window.scrollY;
+
+        if(scrollPoss>0){
+            header.classList.add("fixed");
+        }
+        else{
+            header.classList.remove("fixed");
+        }
+        
     });
 
+    const btn = document.querySelector("#contact");
+    const navLinks = document.querySelectorAll(".nav_link");
 
 
-});
+    for(let navItem of navLinks){
+        navItem.addEventListener("click", function(){
+            console.log('link clicked');
+        });
+    }
